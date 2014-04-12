6582815feeddb5813fd203b43cb95d72ad17ce3d
Morgan Wall <mwall1992@gmail.com>
2014-04-13 08:02:54 +1000

    Incomplete implementation of Stack Exchange API library

--- EducationOverflow/StackExchangeAPI/StackExchangeAPIRequest.cs
+++ EducationOverflow/StackExchangeAPI/StackExchangeAPIRequest.cs
@@ -4,44 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Net;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+using System.IO.Compression;
+
 namespace StackExchangeAPI {
+
     public class StackExchangeAPIRequest {
 
-        private String method;
+        public static ResponseWrapper<T> MakeRequest<T>(StackExchangeAPIRequestInfo requestInfo) {
+            WebRequest request = WebRequest.Create("http://api.stackexchange.com/2.2/answers?order=desc&sort=activity&site=stackoverflow&filter=!Ldk(uYFB0jj2D42wej1Pr5");
+            request.Method = "GET";
 
-        private String parameterPairs;
+            WebResponse response = request.GetResponse();
 
-        private String siteParameter;
+            string json;
+            MemoryStream decompressedStream = new MemoryStream();
+            GZipStream compressedStream = new GZipStream(response.GetResponseStream(), CompressionMode.Decompress);
 
-        public String Method {
-            get {
-                return this.method;
+            compressedStream.CopyTo(decompressedStream);
+            decompressedStream.Seek(0, SeekOrigin.Begin);
+
+            using (StreamReader reader = new StreamReader(decompressedStream, System.Text.Encoding.UTF8)) {
+                json = reader.ReadToEnd();
             }
 
-            set {
-                this.method = value;
-            }
+            DataContractJsonSerializer ser = new DataContractJsonSerializer(typeof(ResponseWrapper<T>));
+            MemoryStream stream = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(json));
+            ResponseWrapper<T> obj = (ResponseWrapper<T>)ser.ReadObject(stream);
+
+            stream.Close();
+            compressedStream.Close();
+            decompressedStream.Close();
+
+            return obj;
         }
-
-        public String ParameterPairs {
-            get {
-                return this.parameterPairs;
-            }
-
-            set {
-                this.parameterPairs = value;
-            }
-        }
-
-        public String SiteParameter {
-            get {
-                return this.siteParameter;
-            }
-
-            set {
-                this.siteParameter = value;
-            }
-        }
-
     }
 }
---
+++ EducationOverflow/StackExchangeAPI/StackExchangeAPIRequestInfo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackExchangeAPI {
+
+    public class StackExchangeAPIRequestInfo {
+
+        /// <summary>
+        /// The base URL for all requests to the Stack Exchange API.
+        /// </summary>
+        private static String baseURL = "http://api.stackexchange.com";
+
+        /// <summary>
+        /// The Stack Exchange API version used.
+        /// </summary>
+        private static String APIVersion = "2.2";
+
+        /// <summary>
+        /// The API method used as specified by the Stack Exchange API documentation.
+        /// </summary>
+        /// <example>
+        /// Every API method string begins with a "/". To query for all the
+        /// answers on a Stack Exchange site, for example, the API method string 
+        /// would be "/answers".
+        /// </example>
+        /// <remarks>
+        /// The Stack Exchange API defines each of its methods as a string which is
+        /// included as part of the URL defining the request to the API servers. For a
+        /// list of all the available methods, see the Stack Exchange API documentation
+        /// at http://api.stackexchange.com/docs
+        /// </remarks>
+        private String apiMethod;
+
+        /// <summary>
+        /// A collection of key-value pair mappings included as parameters to a query 
+        /// sent to the Stack Exchange API servers.
+        /// </summary>
+        /// <remarks>
+        /// The methods of the Stack Exchange API are categorised as either per-site methods
+        /// or network methods. All per-site methods require an api site parameter, which 
+        /// uniquely identifies the Stack Exchange site being queried. For more information
+        /// and a listing of the api site parameters, see the Stack Exchange API documentation
+        /// at http://api.stackexchange.com/docs
+        /// </remarks>
+        private Dictionary<String, String> parameters;
+
+        // properties
+
+        public String APIMethod {
+            get {
+                return this.apiMethod;
+            }
+
+            set {
+                this.apiMethod = value;
+            }
+        }
+
+        public Dictionary<String, String> Parameters {
+            get {
+                return this.parameters;
+            }
+        }
+
+        // methods
+
+        public StackExchangeAPIRequestInfo() {
+            this.parameters = new Dictionary<String, String>();
+        }
+
+        public StackExchangeAPIRequestInfo(Dictionary<String, String> parameters) {
+            this.parameters = parameters;
+        }
+
+        public String ToURL() {
+            return String.Format("{0}/{1}/{2}", baseURL, APIVersion, this.apiMethod);
+        }
+
+        // helper methods
+
+        private String ParametersToString() {
+            return "";
+        }
+    }
+}
